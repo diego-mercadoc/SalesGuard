402483292fa52c70a2f99f4955eca426f671bd32
JuanPab2009 <pablo.blanco@iteso.mx>
2026-05-09 16:33:10 -0600

    Add testing setup and update docs

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  testEnvironment: "node",
+  testMatch: ["**/tests/**/*.test.ts"],
+  setupFiles: ["<rootDir>/tests/setup.ts"],
+  clearMocks: true,
+  transform: {
+    "^.+\\.ts$": [
+      "ts-jest",
+      {
+        tsconfig: "tsconfig.test.json"
+      }
+    ]
+  }
+};
